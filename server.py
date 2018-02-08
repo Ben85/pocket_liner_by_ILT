be5e1f16ee89f6516902b52913b0f1b44f4c0b9c
Bende Hajdu <outdoorsport85@gmail.com>
2018-02-08 11:23:21 +0100

    decorator for login

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
-from flask import Flask, redirect, render_template, url_for, request, session
+from flask import Flask, flash, redirect, render_template, url_for, request, session
 import data_manager
 import validation
+from functools import wraps
 
 app = Flask(__name__)
+
+
+def login_required(function):
+    @wraps(function)
+    def decorated_function(args, **kwargs):
+        if session == {}:
+            flash("For this you need to log in")
+            return redirect(url_for('login', next=request.url))
+        return function(args, **kwargs)
+    return decorated_function
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,27 +32,34 @@ def route_register():
 @app.route('/login', methods=['POST', 'GET'])
 def route_login():
     if request.method == 'POST':
-        session['id'] = data_manager.get_user_id_by_email(request.form['e_mail'])
+        if validation.is_user_pass(request.form['password'], session['id']):
+            session['id'] = data_manager.get_user_id_by_email(request.form['e_mail'])
+        else:
+            raise ValueError
 
-        validation.is_user_pass(request.form['password'], session['id'])
     return redirect(url_for('route_user_page', session['id']))
 
+
 @app.route('/<user_id>')
+@login_required
 def route_user_page(id):
     pass
 
 
 @app.route('/spend')
+@login_required
 def route_spend_money():
     pass
 
 
 @app.route('/incomes')
+@login_required
 def route_incomes():
     pass
 
 
 @app.route('/all-expenses')
+@login_required
 def route_all_expenses():
     pass
 
