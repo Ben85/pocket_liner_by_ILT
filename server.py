d7a04a9bec56a96912544e919c3c063189f95408
Bende Hajdu <outdoorsport85@gmail.com>
2018-02-08 15:52:43 +0100

    login debuged

--- server.py
+++ server.py
@@ -10,16 +10,11 @@ app = Flask(__name__)
 def login_required(function):
     @wraps(function)
     def decorated_function(*args, **kwargs):
-        if session['id'] == {}:
+        if 'user_id' not in session:
             flash("For this you need to log in")
-            return redirect(url_for('/', next=request.url))
+            return redirect(url_for('login', next=request.url))
         return function(*args, **kwargs)
     return decorated_function
-
-
-@app.route('/')
-def route_index():
-    return render_template('index.html')
 
 
 @app.route('/logout')
@@ -34,26 +29,27 @@ def route_register():
     user_data = request.form.to_dict()
     is_email_used = data_manager.email_used(request.form["e_mail_reg"])
     if len(is_email_used) > 0:
-        return render_template('index.html', is_email_used = True)
+        return render_template('login.html', is_email_used = True)
     user_data['password_reg'] = encryption.hash_password(request.form["password_reg"])
     data_manager.register_user(user_data)
     return redirect(url_for('route_index'))
 
 
 @app.route('/login', methods=['POST', 'GET'])
-def route_login():
-    if validation.is_user_pass(request.form['password'], request.form['e_mail']):
-        session['id'] = data_manager.get_user_id_by_email(request.form['e_mail'])
-    else:
-        raise ValueError('Incorrect password or e-mail')
+def login():
+    if request.method == 'POST':
+        if validation.is_user_pass(request.form['password'], request.form['e_mail']):
+            user_data = data_manager.get_user_data_by_e_mail(request.form['e_mail'])
+            session['user_id'] = user_data['id']
+            session['user_name'] = user_data['name']
+            return redirect(url_for('home'))
+    return render_template('login.html')
 
-    return redirect(url_for('route_user_page', user_id=session['id']))
 
-
-@app.route('/<int:user_id>')
+@app.route('/')
 @login_required
-def route_user_page(user_id):
-    return render_template('user_index.html', user_id=user_id)
+def home():
+    return render_template('user_index.html', user_id=session['user_id'], user_name=session['user_name'])
 
 
 @app.route('/spend')
